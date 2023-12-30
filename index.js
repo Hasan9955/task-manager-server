b795a25b3c2fa6c348ca0305beba99a6a76c5fa1
Hasan9955 <hasanbinali7556@gmail.com>
2023-12-30 20:59:26 +0600

    post user data

--- index.js
+++ index.js
@@ -11,13 +11,54 @@ app.use(cors())
 app.use(express.json())
 
 
+// mongoDB Setup
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udflnrf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const userCollection = client.db("TaskManager").collection("users")
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const query = { email: user.email } 
+      const isExist = await userCollection.findOne(query)
+      if(isExist){
+        return res.send({massage: 'user is already exist'})
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Express cargo server is running !!!')
+    res.send('Task manager server is running !!!')
 })
 
 app.all('*', (req, res, next) => {
@@ -39,5 +80,5 @@ app.use((err, req, res, next) => {
 
 
 app.listen(port, () => {
-    console.log(`Express Cargo server is running on port : ${port}`)
+    console.log(`Task manager server is running on port : ${port}`)
 })
